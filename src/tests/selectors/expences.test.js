2ada8a089dff5c9442a70a594e578455d875f1ce
ANISDHIYAEDDINE <dhiaeboudiaf@gmail.com>
2020-07-01 01:02:46 +0100

    basic selector testing

---
+++ src/tests/selectors/expences.test.js
@@ -0,0 +1,84 @@
+import selector from "../../selectors/expenses";
+import moment from "moment";
+const expences = [
+  {
+    id: "1",
+    description: "Hello",
+    note: "",
+    amount: 10,
+    createdAt: 0,
+  },
+  {
+    id: "2",
+    description: "high tech",
+    note: "",
+    amount: 0,
+    createdAt: moment(0).subtract(4, "days").valueOf(),
+  },
+  {
+    id: "3",
+    description: "yepp",
+    note: "",
+    amount: 200,
+    createdAt: moment(0).add(4, "days").valueOf(),
+  },
+];
+
+test("filter by text", () => {
+  let filters = {
+    text: "h",
+    sortBy: "date",
+    startDate: moment(0).subtract(10, "days").valueOf(),
+    endDate: moment(0).add(10, "days").valueOf(),
+  };
+  let result = selector(expences, filters);
+  expect(result).toEqual([expences[0], expences[1]]);
+});
+
+test("filter by startDate", () => {
+  let filters = {
+    text: "",
+    sortBy: "date",
+    startDate: moment(0).subtract(2, "days").valueOf(),
+    endDate: moment(0).add(10, "days").valueOf(),
+  };
+
+  let result = selector(expences, filters);
+  expect(result).toEqual([expences[2], expences[0]]);
+});
+
+test("filter by EndDate", () => {
+  let filters = {
+    text: "",
+    sortBy: "date",
+    startDate: moment(0).subtract(10, "days").valueOf(),
+    endDate: moment(0).add(2, "days").valueOf(),
+  };
+
+  let result = selector(expences, filters);
+  expect(result).toEqual([expences[0], expences[1]]);
+});
+
+test("filter by all filters", () => {
+  let filters = {
+    text: "h",
+    sortBy: "date",
+    startDate: moment(0).subtract(2, "days").valueOf(),
+    endDate: moment(0).add(2, "days").valueOf(),
+  };
+
+  let result = selector(expences, filters);
+  expect(result).toEqual([expences[0]]);
+});
+
+test("sort by amount", () => {
+  let filters = {
+    text: "",
+    sortBy: "amount",
+    startDate: moment(0).subtract(10, "days").valueOf(),
+    endDate: moment(0).add(10, "days").valueOf(),
+  };
+
+  let result = selector(expences, filters);
+  expect(result).toEqual([expences[2], expences[0], expences[1]]);
+});
